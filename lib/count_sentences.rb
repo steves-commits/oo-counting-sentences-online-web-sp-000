d65272c8ba10159c6f394447acd95d3254c7c1b8
Steven Topazio <sat@northpointprinting.com>
2020-09-30 19:26:25 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,7 +29,7 @@ class String
   end
 
   def count_sentences
-    new_array = self.split(/.?!/).reject {|string| string.empty?}
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
     return new_array.length
   end
 end
